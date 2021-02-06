25a59c46962e613f7249c238a8ddb5fe1077abae
travis_gandy <travis_gandy@yahoo.com>
2021-02-06 00:41:48 -0500

    generate song resource

---
+++ db/migrate/20210206054043_create_songs.rb
@@ -0,0 +1,12 @@
+class CreateSongs < ActiveRecord::Migration[6.0]
+  def change
+    create_table :songs do |t|
+      t.string :title
+      t.integer :length
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :album, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
