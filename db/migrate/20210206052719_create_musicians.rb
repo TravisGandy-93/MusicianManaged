95308b0e5dd76b0ca1065ac6f14ccbfd802af79c
travis_gandy <travis_gandy@yahoo.com>
2021-02-06 00:27:43 -0500

    generate musician resource

---
+++ db/migrate/20210206052719_create_musicians.rb
@@ -0,0 +1,11 @@
+class CreateMusicians < ActiveRecord::Migration[6.0]
+  def change
+    create_table :musicians do |t|
+      t.string :name
+      t.string :instrument
+      t.integer :members
+
+      t.timestamps
+    end
+  end
+end
