334b2b9c61d99a300349e4716824ff39f351005b
travis_gandy <travis_gandy@yahoo.com>
2021-02-06 11:13:30 -0500

    adding basic welcome page where login/signup will take place

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :albums
   resources :musicians
   resources :users
+
+  get '/' => 'sessions#welcome'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
