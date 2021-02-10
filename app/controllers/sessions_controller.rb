bac852f5720f7078aca6ec697f23690dd2a7510d
travis_gandy <travis_gandy@yahoo.com>
2021-02-09 21:07:37 -0500

    adding delete logout route and destroy action to sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,8 @@
 class SessionsController < ApplicationController
     def welcome
     end
+
+    def destroy
+        session.delete[:user_id]
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   post 'login' => 'sessions#create'
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
+
+  delete '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
