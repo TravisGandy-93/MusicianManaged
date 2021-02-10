afefc5ce06d3892db00f5d0c59ba8dc9ae94c0c4
travis_gandy <travis_gandy@yahoo.com>
2021-02-09 20:48:14 -0500

    fixing mistake in user, changing from username to email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_many :songs
     has_many :albums, through: :songs
-    validates :username, uniqueness: true, presence: true
+    validates :email, uniqueness: true, presence: true
     has_secure_password
 
 end
