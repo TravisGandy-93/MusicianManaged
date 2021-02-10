74361e2e74020d13f7f23028aa04b43c6aa5c34e
travis_gandy <travis_gandy@yahoo.com>
2021-02-09 21:40:08 -0500

    added new and create action to sessions controller and login form in sessions new view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,5 +4,20 @@ class SessionsController < ApplicationController
 
     def destroy
         session.delete[:user_id]
+        redirect_to '/'
+    end 
+
+    def new
+    end 
+
+    def create
+        @user = User.find_by(email: params[:user][:email])
+        if @user.try(:authenticate, params[:user][:email])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash[:error] = "Sorry that's a bad combination"
+            redirect_to login_path
+        end
     end 
 end
