c4d9cca2a7c4ff7dd9d87ab4273914b76d46cfe1
travis_gandy <travis_gandy@yahoo.com>
2021-02-11 05:47:32 -0500

    Finally fixed server issues. migrated tables and fixed sign up link

--- db/migrate/20210206053529_create_albums.rb
+++ db/migrate/20210206053529_create_albums.rb
@@ -1,7 +1,7 @@
 class CreateAlbums < ActiveRecord::Migration[6.0]
   def change
     create_table :albums do |t|
-      t.varbinary :artwork
+      t.binary :artwork
       t.string :genre
       t.date :release_date
       t.belongs_to :musician, null: false, foreign_key: true
---
+++ db/schema.rb
@@ -0,0 +1,54 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_02_06_054043) do
+
+  create_table "albums", force: :cascade do |t|
+    t.binary "artwork"
+    t.string "genre"
+    t.date "release_date"
+    t.integer "musician_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["musician_id"], name: "index_albums_on_musician_id"
+  end
+
+  create_table "musicians", force: :cascade do |t|
+    t.string "name"
+    t.string "instrument"
+    t.integer "members"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "title"
+    t.integer "length"
+    t.integer "user_id", null: false
+    t.integer "album_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["album_id"], name: "index_songs_on_album_id"
+    t.index ["user_id"], name: "index_songs_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "albums", "musicians"
+  add_foreign_key "songs", "albums"
+  add_foreign_key "songs", "users"
+end
