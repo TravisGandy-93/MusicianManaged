013e5680d1f334bdfbb8ec33933e08e44d5429f3
travis_gandy <travis_gandy@yahoo.com>
2021-02-13 07:41:59 -0500

    putting together a form that can create a song and the musician and album it belongs to

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,20 +2,23 @@ class SongsController < ApplicationController
     def new
         @song = Song.new
         @song.build_musician
+        @song.save
     end 
 
     def create
         @song = Song.new(song_params)
-        if @song.save
-       redirect_to song_path
+        @song.user_id = session[:user_id]
+        
+        if @song.save!
+       redirect_to song_path(@song)
         else
-      redirect_to :new
+       redirect_to new_song_path
         end
     end
 
     private
 
     def song_params
-        params.require(:song).permit(:title, :length, :musician_id, musician_attributes: [:name])
+        params.require(:song).permit(:title, :length, :musician_id, musician_attributes: [:name], musician_attributes: [:instrument], :album_id)
     end 
 end
