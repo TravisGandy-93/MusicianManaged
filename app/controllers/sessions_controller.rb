2baf025549b08b2c1a3c249345106a642dfd2a7e
travis_gandy <travis_gandy@yahoo.com>
2021-02-17 10:10:43 -0500

    adding/testing omniauth for facebook login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,17 +7,27 @@ class SessionsController < ApplicationController
         redirect_to '/'
     end 
 
-    def new
+    def create
+      #  @user = User.find_by(email: params[:user][:email])
+     #   if @user.try(:authenticate, params[:user][:password])
+     #       session[:user_id] = @user.id
+     #       redirect_to user_path(@user)
+     #   else
+     #       flash[:error] = "Sorry that's a bad combination"
+     #       redirect_to login_path
+      #  end
     end 
 
-    def create
-        @user = User.find_by(email: params[:user][:email])
-        if @user.try(:authenticate, params[:user][:password])
-            session[:user_id] = @user.id
-            redirect_to user_path(@user)
-        else
-            flash[:error] = "Sorry that's a bad combination"
-            redirect_to login_path
-        end
-    end 
+    def omniauth
+        @user = User.create_by_facebook_omniauth(auth)
+    
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+    end
+
+    private
+
+    def auth
+        request.env['omniauth.auth']
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :songs
 
   get '/' => 'sessions#welcome'
+  get '/auth/:provider/callback', to: 'sessions#create'
 
   get '/login' => 'sessions#new'
   post 'login' => 'sessions#create'
@@ -14,7 +15,7 @@ Rails.application.routes.draw do
 
   get "/albums/:id/songs/new" => 'songs#new'
   post "/albums/:id/songs/new" => 'songs#create'
-  
+
   get '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
