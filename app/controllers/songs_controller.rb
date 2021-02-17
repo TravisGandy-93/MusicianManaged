3c8040d4c3438aab38b136d92ae9f91bd09bca75
travis_gandy <travis_gandy@yahoo.com>
2021-02-16 21:03:52 -0500

    songs create and new actions plus form and custom route

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,25 @@
 class SongsController < ApplicationController
+    def new 
+        @song = Song.new
+    end 
+
+    def create
+        song = Song.new(song_params)
+        
     
+        if song.save!
+            redirect_to songs_path
+       else
+            redirect_to new_song_path
+       end 
+    end
+
+    private
+
+    def song_params
+        params.require(:song).permit(:title, :length, :user_id, :album_id, :musician_id)
+    end
+
 end
 
-#, :album_id, album_attributes: [:name, :release_date], musician_attributes: [:name, :instrument]
+#, :album_id, album_attributes: [:name, :release_date], song_attributes: [:name, :instrument]
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,8 @@ Rails.application.routes.draw do
   post '/signup' => 'users#create'
 
   get "/albums/:id/songs/new" => 'songs#new'
- 
+  post "/albums/:id/songs/new" => 'songs#create'
+  
   get '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
