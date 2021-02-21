05de4e518ce66dae35af8cbecf1d45cbe8bc70d6
travis_gandy <travis_gandy@yahoo.com>
2021-02-21 13:11:39 -0500

    scope method to order albums by creation

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,5 +2,9 @@ class Album < ApplicationRecord
   has_many :songs, :dependent => :destroy
   belongs_to :musician
   validates :name, presence: true
+
+  scope :recent, -> { order(:created_at, :desc) }
+
+  
  
 end
